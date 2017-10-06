b052c92624e6dc179feb4588329fd53c69eef5f7
INTIGRA <info@intigratech.com>
2017-10-06 20:27:03 +0530

    Initial commit

---
+++ partner_sales_button/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Customer Sales Button Counter Remove',
+    'version': '1.0',
+    'author': 'INTIGRA TECHNOLAB',
+    'category': 'Base',
+    'description': """Customer Sales Button Counter Remove.""",
+    'website':'www.intigratech.com', 
+    'depends': ['base','sale','sales_team'],
+    'data': [
+        'views/res_partner.xml',             
+    ],
+    'installable': True,
+    'auto_install': False,  
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
